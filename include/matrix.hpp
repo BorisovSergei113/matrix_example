6ca2acc5b30b693068733e5ca5fb2b417c6a33e7
BorisovSergei113 <borisov_sergei113@mail.ru>
2018-03-07 14:03:13 +0300

    test

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -2,26 +2,27 @@
 
 class matrix_t {
 private:
-	float ** elements_;
-	std::size_t rows_;
-	std::size_t collumns_;
+    int ** data;
+    unsigned int stroki;
+    unsigned int stolbi;
 public:
-	matrix_t();
-	matrix_t( matrix_t const & other );
-	matrix_t & operator =( matrix_t const & other );
-	~matrix_t();
-
-	std::size_t rows() const;
-	std::size_t collumns() const;
-
-	matrix_t operator +( matrix_t const & other ) const;
-	matrix_t operator -( matrix_t const & other ) const;
-	matrix_t operator *( matrix_t const & other ) const;
-
-	matrix_t & operator -=( matrix_t const & other );
-	matrix_t & operator +=( matrix_t const & other );
-	matrix_t & operator *=( matrix_t const & other );
-
-	std::istream & read( std::istream & stream );
-	std::ostream & write( std::ostream  & stream ) const;
+    matrix_t();
+    matrix_t(int in_stroki,int in_stolbi);
+    matrix_t( matrix_t const & object );
+    matrix_t & operator =( matrix_t const & other );
+    ~matrix_t();
+    
+    unsigned int Get_Stroki() const;
+    unsigned int Get_Stolbi() const;
+    
+    matrix_t operator +( matrix_t const & other ) const;
+    matrix_t operator -( matrix_t const & other ) const;
+    matrix_t operator *( matrix_t const & other ) const;
+    
+    matrix_t & operator -=( matrix_t const & other );
+    matrix_t & operator +=( matrix_t const & other );
+    matrix_t & operator *=( matrix_t const & other );
+    
+    std::istream & read( std::istream & stream );
+    std::ostream & write( std::ostream  & stream ) const;
 };
